989973417cbbeda49795f17cf7746d59b46378a3
tamzi <tamrefrank@gmail.com>
2020-10-30 13:57:38 +0300

    Added arrayCreator.py file

---
+++ arrayCreator.py
@@ -0,0 +1,6 @@
+###  This is an Alogorithm that will take in an input of integers.
+# It then adds all the elements in the arrays(List).
+input_string = input("Enter some numbers separated by space: ")
+
+inputList = input_string.split()
+print("The list created is ", inputList)
